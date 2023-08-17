b48f1f1800e9d3e39a2132374a71826caab529c0
Gerard Kamran Kalambacal <kamrangerardkalambacal@gmail.com>
2023-08-17 20:32:07 +0800

    test: added test case should_change_direction_to_West_when_executeCommand_given_initial_direction_South_and_command_TurnRight

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -156,4 +156,19 @@ class MarsRoverTest {
         // Then
         Assertions.assertEquals(Direction.SOUTH, currentLocation.getDirection());
     }
+
+    @Test
+    void should_change_direction_to_West_when_executeCommand_given_initial_direction_South_and_command_TurnRight() {
+        // Given
+        Location initialLocation = new Location(0, 0, Direction.EAST);
+        Command givenCommand = Command.TURN_RIGHT;
+        MarsRover marsRover = new MarsRover(initialLocation);
+
+        // When
+        marsRover.executeCommand(givenCommand);
+        Location currentLocation = marsRover.getCurrentLocation();
+
+        // Then
+        Assertions.assertEquals(Direction.SOUTH, currentLocation.getDirection());
+    }
 }
