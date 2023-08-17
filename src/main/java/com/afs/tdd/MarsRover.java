c6fd6ac39ac4bebca5703bf92bb0155f389c04f4
Gerard Kamran Kalambacal <kamrangerardkalambacal@gmail.com>
2023-08-17 19:41:56 +0800

    feat: added turnLeft method

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -2,13 +2,14 @@ package com.afs.tdd;
 
 public class MarsRover {
 
-    private Location location;
+    private final Location location;
 
     public MarsRover(Location location) {
         this.location = location;
     }
 
     public void executeCommand(Command givenCommand) {
+        Direction newDirection = null;
         if(givenCommand == Command.MOVE) {
             if(location.getDirection() == Direction.NORTH){
                 location.setY(location.getY() + 1);
@@ -23,6 +24,26 @@ public class MarsRover {
                 location.setX(location.getX() + 1);
             }
         }
+        if (givenCommand == Command.TURN_LEFT) {
+            newDirection = turnLeft();
+        }
+    }
+
+    private Direction turnLeft() {
+        Direction currentDirection = location.getDirection();
+        Direction newDirection = null;
+
+        if (currentDirection == Direction.NORTH) {
+            newDirection = Direction.WEST;
+        } else if (currentDirection == Direction.EAST) {
+            newDirection = Direction.NORTH;
+        } else if (currentDirection == Direction.SOUTH) {
+            newDirection = Direction.EAST;
+        } else if (currentDirection == Direction.WEST) {
+            newDirection = Direction.SOUTH;
+        }
+
+        return newDirection;
     }
 
     public Location getCurrentLocation() {
